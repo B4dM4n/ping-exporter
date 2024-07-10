7c2e37f090569e9892dc2806dcc352f970871e88
Fabian Möller <fabianm88@gmail.com>
2024-07-10 16:21:27 +0200

    flake.nix: Use rust-overlay with musl target

--- flake.nix
+++ flake.nix
@@ -2,24 +2,30 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
     self,
     flake-utils,
     nixpkgs,
+    rust-overlay,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = nixpkgs.legacyPackages.${system}.extend rust-overlay.overlays.default;
+
+      rustStable = pkgs.rust-bin.stable.latest.default.override {
+        extensions = ["rust-src"];
+        targets = ["x86_64-unknown-linux-musl"];
+      };
     in {
       devShell = pkgs.mkShell {
         preferLocalBuild = true;
 
         buildInputs = with pkgs; [
-          cargo
-          clippy
-          rustc
+          rustStable
         ];
       };
     });
