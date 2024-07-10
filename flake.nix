25088b201aedd9810bbf4f84b44570e0101f94b8
Fabian Möller <fabianm88@gmail.com>
2024-07-10 11:30:49 +0200

    Remove rust-overlay from flake.nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,6 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    rust-overlay.inputs.flake-utils.follows = "flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
@@ -11,17 +8,10 @@
     self,
     flake-utils,
     nixpkgs,
-    rust-overlay,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system}.extend rust-overlay.overlays.default;
-
-      rust-nightly = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
-
-      rustfmt-nightly = pkgs.writeShellScriptBin "rustfmt" ''
-        exec ${rust-nightly}/bin/rustfmt "$@"
-      '';
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShell = pkgs.mkShell {
         preferLocalBuild = true;
@@ -30,7 +20,6 @@
           cargo
           clippy
           rustc
-          rustfmt-nightly
         ];
       };
     });
